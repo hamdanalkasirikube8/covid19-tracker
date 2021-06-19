5b0d9a433badcd6a01e5e9aac550ca424caab2ed
HamdanAlkasiri <hamdan.alkasiri@stud.uni-bamberg.de>
2021-06-19 17:16:47 +0200

    test

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static("./dist/covid-tracker"));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/covid-tracker'));
 
-app.get("/*", function (req, res) {
-    res.sendFile("index.html", { root: "dist/covid-tracker" });
+app.all('*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/covid-tracker/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
