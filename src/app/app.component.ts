5ebc0feabc39ea6c86b21872fae0f7efb6490930
HamdanAlkasiri <hamdan.alkasiri@stud.uni-bamberg.de>
2021-06-20 12:00:01 +0200

    Map issue resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,15 @@ import { MediaMatcher } from '@angular/cdk/layout';
 export class AppComponent {
   private _mobileQueryListener: () => void;
   mobileQuery: MediaQueryList;
-
+  opened: boolean;
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
-
+  ngOnInit(): void {
+    this.opened = true;
+  }
 }
 
 
